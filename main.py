62d656ea46e082a3f4954817eca9c0ab876b2c85
Dauren Bekenov <87059069724@mail.ru>
2022-02-22 21:52:05 +0600

    my first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+class Bird:
+    def __init__(self, range_flight, flight_altitude, stomach_capacity):
+        self.range_flight = range_flight
+        self.flight_altitude = flight_altitude
+        self.stomach_capacity = stomach_capacity
+
+def display_max_range(self):
+    #Отабразим макс дальность полета
+    print(f"На данный момент максимальная дальность полета: {self.range_flight}")
+
+def set_max_range(self, range_flight):
+    #Устанавливаем макс дальность полета
+    self.range.flight = range_flight
+
+def display_max_altitude(self):
+    #отабразим макс высоту
+    print(f"На данный момент птичка находится на высоте: {self.flight_altitude}")
+
+def set_max_flight_altitude(self, flight_altitude):
+    #устанавливаем макс высоту полета
+    self.flight_altitude = flight_altitude
